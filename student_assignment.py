b143c54f2f2d5d95d824ad005db653bb7a5d6305
Kyle Wang <kyle_wang@asus.com>
2025-03-11 17:37:13 +0800

    hw2

--- student_assignment.py
+++ student_assignment.py
@@ -40,7 +40,20 @@ def generate_hw01():
     return collection
     
 def generate_hw02(question, city, store_type, start_date, end_date):
-    pass
+    collection = generate_hw01()
+    ret = collection.query(
+        query_texts=[question],
+        n_results=10,
+        where={"$and" : [
+                {"city": {"$in": city}},
+                {"type": {"$in": store_type}},
+                {"date": {"$gte": start_date.timestamp()}},
+                {"date": {"$lte": end_date.timestamp()}},
+        ]},
+        include=["metadatas", "distances"],
+    )
+    t = zip(ret["distances"][0], ret["metadatas"][0])
+    return [i[1]["name"] for i in t if i[0] <= 1 - 0.8]
     
 def generate_hw03(question, store_name, new_store_name, city, store_type):
     pass
@@ -63,4 +76,5 @@ def demo(question):
     return collection
 
 if __name__ == "__main__":
-    generate_hw01()
+    #generate_hw01()
+    print(generate_hw02("我想要找有關茶餐點的店家", ["宜蘭縣", "新北市"], ["美食"], datetime.datetime(2024, 4, 1), datetime.datetime(2024, 5, 1)))
